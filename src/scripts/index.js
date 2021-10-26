e3e7a6f22d49c5882bbb9892632616fa78477025
Adam Chandra Setianugraha <adamchandra890@gmail.com>
2021-10-27 01:35:35 +0700

    add off canvas manu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,23 @@ import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
 const data = require("../DATA.json");
 
+const hamburgerElement = document.querySelector("#hamburger");
+const navElement = document.querySelector("nav");
+const heroElement = document.querySelector(".hero");
+const mainElement = document.querySelector("main");
+
+hamburgerElement.addEventListener("click", (event) => {
+  navElement.classList.add("open");
+  event.stopPropagation();
+});
+
+heroElement.addEventListener("click", () => {
+  navElement.classList.remove("open");
+});
+mainElement.addEventListener("click", () => {
+  navElement.classList.remove("open");
+});
+
 const content = document.querySelector(".content");
 for (const iterator of data.restaurants) {
   const articel = document.createElement("article");
